af47fc05ed487c44370ec81f8329437a87439d4a
Marc <marc@synapse-medicine.com>
2022-06-27 18:09:24 +0200

    [FIX] add valid signature to override pipe_to_target

--- smart_on_fhir_client/requester/fhir_resource.py
+++ smart_on_fhir_client/requester/fhir_resource.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from fhirpy.lib import AsyncFHIRResource
 from loguru import logger
 
@@ -32,7 +34,9 @@ class CustomFHIRResource(SerializeMixin, AsyncFHIRResource):
         resource = await client_proxy.search(identifier=identifier_value).first()
         return resource.id if resource is not None else None
 
-    async def pipe_to_target_fhir_server(self, target_identifier_url: str = None):
+    async def pipe_to_target_fhir_server(
+        self, *, target_identifier_url: str = None, **kwargs: Any
+    ) -> "CustomFHIRResource":
         client_proxy = getattr(self.target_requester, self.resource_type)
         # try to find the resource on the target fhir server
         resource_id = await self.find_by_identifier(target_identifier_url, client_proxy)
